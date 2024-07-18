5a8ed0f74a7e7cfce416be957d460c899a9b369a
Lucas-Henschel <lucas101henschel@gmail.com>
2024-07-18 20:37:24 -0300

    Project created

---
+++ src/test/java/com/technosdev/TechnosdevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technosdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnosdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
